46c3fd780553fa34d4a1e36c02f5b886b81e467b
Gelo0321 <abmanalang.student@ua.edu.ph>
2023-09-14 16:47:01 +0800

    aaa

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,17 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-      <p>My First impression for the 1st month as a 3rd year CpE student.</p>
-        <p>  </p>
-        <p>As a 3rd yr. CpE student, my 1st month in this yr. is very hard and stressed because 
-          of our schedule, and transportation. But our major subjects are very challenging and I enjoying doing it. I learned some knowledge about programming, and how to use Visual studio, gitbush..</p>
-          <p> were trying to build application </p>
-          <p> we know now how to update a website </p>
-
+      <p>10 Things That Require Zero Talent</p>
+          <p> 1. Being On Time. </p>
+          <p> 2. Making An Effort. </p>
+          <p> 3. Being High Energy. </p>
+          <p> 4. Having A Positive Attitude. </p>
+          <p> 5. Being Passionate. </p>
+          <p> 6. Using Good Body Language. </p>
+          <p> 7. Being Coachable. </p>
+          <p> 8. Doing A Little Extra. </p>
+          <p> 9. Being Prepared. </p>
+          <p> 10. Having A Strong Work Ethic. </p>
       </section>
     </Layout>
   );
